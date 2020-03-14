80b7dee9c1a0dbba8771b6c8fa7e106d2a884093
Csar Hernndez Rocha <cesarhrocha96@gmail.com>
2020-03-14 11:31:32 -0600

    masca

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
 import { from } from 'rxjs';
 import { interval } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 
 const data = from(fetch('https://server.hexbird.mx:3001/api/tasks/getTasks'));
+const apiData = ajax('https://server.hexbird.mx:3001/api/tasks/getTasks');
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -12,18 +15,28 @@ const data = from(fetch('https://server.hexbird.mx:3001/api/tasks/getTasks'));
 export class AppComponent {
   title = 'rxjs-tests';
 
-  // tslint:disable-next-line: use-lifecycle-interface
   ngOnInit() {
+    this.test3();
+  }
+
+  test1() {
     data.subscribe({
       next(response) { console.log(response.body); },
       error(err) { console.error('Error: ' + err); },
       complete() { console.log('Completed'); }
     });
+  }
 
+  test2() {
     const secondsCounter = interval(1000);
     // Subscribe to begin publishing values
     secondsCounter.subscribe(n =>
       console.log(`It's been ${n} seconds since subscribing!`));
+  }
+
+  test3() {
+    apiData.subscribe(res => console.log(res.status, res.response));
+
   }
 
 }
